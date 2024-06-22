4b46a976b24c4d34730dcf0062c275bfe2d6eed6
Thisara <sasmithajayasinghe1@gmail.com>
2024-06-22 21:05:48 +0530

    all install

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProjectList from '../ProjectList/ProjectList'
+
+export default function Home() {
+  return (
+    <div>
+        <ProjectList/>
+    </div>
+  )
+}
---
+++ frontend/src/pages/ProjectList/ProjectList.jsx
@@ -0,0 +1,19 @@
+import { Card } from '@/components/ui/card'
+import React from 'react'
+
+export default function ProjectList() {
+  return (
+    <>
+        <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
+            <section className='filterSection'>
+                <Card>
+                    
+                </Card>
+            </section>
+            <section className='projectListSection'>
+
+            </section>
+        </div>
+    </>
+  )
+}
