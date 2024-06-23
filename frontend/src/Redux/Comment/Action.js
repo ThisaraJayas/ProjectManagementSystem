0a638056d376434b2f0320616838abadc05f30fa
Thisara <sasmithajayasinghe1@gmail.com>
2024-06-23 16:46:05 +0530

    comment endoints complete

---
+++ frontend/src/Redux/Comment/Action.js
@@ -0,0 +1,53 @@
+import api from "@/config/api"
+import * as actionTypes from "./ActionTypes"
+
+export const createComment = (commentData)=>{
+    
+    return async(dispatch)=>{
+        dispatch({type: actionTypes.CREATE_COMMENT_REQUEST})
+        try{
+
+            const response = await api.post("/api/comments",commentData)
+            console.log("comment created ",response.data);
+            dispatch({type: actionTypes.SEND_MESSAGES_SUCCESS, comment: response.data})
+        }catch(error){
+            console.log("error ",error);
+            dispatch({type:actionTypes.CREATE_COMMENT_FAILURE, error: error.message})
+        }
+
+    }
+}
+
+export const deleteComment = (commentId)=>{
+    
+    return async(dispatch)=>{
+        dispatch({type: actionTypes.DELETE_COMMENT_REQUEST})
+        try{
+
+            const response = await api.delete(`/api/comments/${commentId}`)
+            console.log("comment deleted ",response.data);
+            dispatch({type: actionTypes.DELETE_COMMENT_SUCCESS, commentId})
+        }catch(error){
+            console.log("error ",error);
+            dispatch({type:actionTypes.DELETE_COMMENT_FAILURE, error: error.message})
+        }
+
+    }
+}
+
+export const fetchComments = (issueId)=>{
+    
+    return async(dispatch)=>{
+        dispatch({type: actionTypes.FETCH_COMMENTS_REQUEST})
+        try{
+
+            const response = await api.get(`/api/comments/${issueId}`)
+            console.log("fetched comments ",response.data);
+            dispatch({type: actionTypes.FETCH_COMMENTS_SUCCESS, comments: response.data})
+        }catch(error){
+            console.log("error ",error);
+            dispatch({type:actionTypes.FETCH_COMMENTS_FAILURE, error: error.message})
+        }
+
+    }
+}
--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -3,11 +3,13 @@ import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
 import { projectReducer } from "./Project/Reducer";
 import { ChatReducer } from "./Chat/Reducer";
+import { commentReducer } from "./Comment/Reducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer,
     chat: ChatReducer,
+    comment: commentReducer,
 })
 export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
 
